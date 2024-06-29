a6e8501e28b982374cd984f9f93822edfc3dd41b
nkaumov <nkaumov7342@gmail.com>
2024-06-29 17:07:58 +0400

    normyal' registr

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,17 @@ router.get('/classes', async (req, res) => {
   }
 });
 
+router.get('/subjects', async (req, res) => {
+  try {
+    const [subjects] = await sequelize.query('SELECT name FROM subjects');
+    res.json(subjects);
+  } catch (error) {
+    console.error('Ошибка загрузки предметов:', error);
+    res.status(500).json({ message: 'Ошибка загрузки предметов' });
+  }
+});
+
+
 router.get('/register', (req, res) => {
   res.render('auth/register', { title: 'Регистрация' });
 });
@@ -51,7 +62,7 @@ router.post('/register', async (req, res) => {
         const [subject] = await sequelize.query('SELECT id FROM subjects WHERE name = ?', {
           replacements: [subjectName],
         });
-
+    
         let subjectId;
         if (subject.length > 0) {
           subjectId = subject[0].id;
@@ -61,12 +72,13 @@ router.post('/register', async (req, res) => {
           });
           subjectId = newSubject[0].id;
         }
-
+    
         await sequelize.query('INSERT INTO user_subjects (user_id, subject_id) VALUES (?, ?)', {
           replacements: [user[0].id, subjectId],
         });
       }
     }
+    
 
     if (role === 'student') {
       let classId;
