0eb37869ec97f487219701f6b10bc138c7c837cc
Sofia <sofiamint1995@gmail.com>
2024-07-01 20:05:06 +0400

    created logic for the groups

--- routes/group.js
+++ routes/group.js
@@ -11,53 +11,102 @@ router.get('/', async (req, res) => {
   } catch (error) {
     console.error('Ошибка при получении данных из базы:', error);
     res.status(500).send('Ошибка сервера');
-<<<<<<< HEAD
   }
+});
+
+// Открытие страницы для создания новой группы
+router.get('/new', (req, res) => {
+  res.render('groups/new');
 });
 
 // Создание новой группы
-router.post('/', async (req, res) => {
-  const { student_id, group_name, study_year, tutor_id } = req.body;
-  await sequelize.query(
-    'INSERT INTO groups (student_id, group_name, study_year, tutor_id) VALUES (?, ?, ?, ?)',
-    { replacements: [student_id, group_name, study_year, tutor_id] }
-  );
-  res.status(201).json({ message: 'Group created' });
-});
+router.post('/new', async (req, res) => {
+  try {
+    const { name } = req.body;
 
-// Получение конкретной группы по ID
-router.get('/:id', async (req, res) => {
-  const [groups, metadata] = await sequelize.query('SELECT * FROM classes WHERE class_id = ?', {
-    replacements: [req.params.id],
-  });
-  const group = groups[0];
-  if (group) {
-    res.json(group);
-  } else {
-    res.status(404).send('Group not found');
+    if (!name) {
+      return res.status(400).json({ message: 'Требуется ввести название группы!' });
+    }
+
+    await sequelize.query(
+      'INSERT INTO classes (name) VALUES (?)',
+      { replacements: [name] }
+    );
+
+    res.json({ message: 'Группа успешно создана!' });
+  } catch (error) {
+    console.error('Ошибка при получении данных из базы:', error);
+    res.status(500).json({ message: 'Ошибка: не удалось создать группу!' });
   }
 });
 
-// Обновление существующей группы
-router.put('/:id', async (req, res) => {
-  const { student_id, group_name, study_year, tutor_id } = req.body;
-  await sequelize.query(
-    'UPDATE groups SET student_id = ?, group_name = ?, study_year = ?, tutor_id = ? WHERE group_id = ?',
-    { replacements: [student_id, group_name, study_year, tutor_id, req.params.id] },
-  );
-  res.json({ message: 'Group updated' });
+
+// Отображение страницы редактирования группы
+router.get('/:id/edit', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [[group]] = await sequelize.query(
+      'SELECT * FROM classes WHERE id = ?',
+      { replacements: [id] }
+    );
+    
+    res.render('groups/edit', {
+      title: 'Редактирование группы',
+      group: group,
+    });    
+  } catch (error) {
+    console.error('Ошибка при получении данных из базы:', error);
+    res.status(500).send('Ошибка сервера');
+  }
+});
+
+// Редактирование данных группы
+router.post('/:id', async (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  try {
+    if (!name) {
+      return res.status(400).json({ message: 'Требуется ввести данные для изменения!' });
+    }
+
+    await sequelize.query(
+      'UPDATE classes SET name = ? WHERE id = ?',
+      { replacements: [name, id] }
+    );
+
+    const [updatedGroup] = await sequelize.query(
+      'SELECT * FROM classes WHERE id = ?',
+      { replacements: [id] }
+    );
+
+    res.json({
+      message: 'Данные группы успешно обновлены!',
+      group: updatedGroup,
+    });
+  } catch (error) {
+    console.error('Ошибка при получении данных из базы:', error);
+    res.status(500).json({ message: 'Ошибка: данные группы не удалось изменить!' });
+  }
 });
 
 // Удаление группы
-router.delete('/:id', async (req, res) => {
-  await sequelize.query('DELETE FROM groups WHERE group_id = ?', { replacements: [req.params.id] });
-  res.status(204).send();
-});
-=======
+router.post('/:id/delete', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await sequelize.query(
+      'DELETE FROM classes WHERE id = ?',
+      { replacements: [id] }
+    );
+
+    res.json({ message: 'Группа успешно удалена!' });
+  } catch (error) {
+    console.error('Ошибка при удалении данных из базы:', error);
+    res.status(500).json({ message: 'Ошибка: не удалось удалить группу!' });
   }
 });
-
-
 
 // Получение конкретной группы по ID
 router.get('/:id', async (req, res) => {
@@ -86,14 +135,12 @@ router.get('/:id', async (req, res) => {
     res.render('groups/show', {
       groupName: group.name,
       students,
+      group: group,
     });
   } catch (error) {
     console.error('Ошибка при получении данных из базы:', error);
     res.status(500).send('Ошибка сервера');
   }
 });
-
-
->>>>>>> 83b5098eeaa79181bea8145a71cca72d90b72743
 
 module.exports = router;
